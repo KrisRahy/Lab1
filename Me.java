153184961465729b89e9709a3ff9859a7bbc929b
Kris Rahy <rahykris4@hotmail.com>
2023-01-25 20:37:38 +0200

    My year of study added

--- Me.java
+++ Me.java
@@ -1,7 +1,8 @@
-public class Helloworld {
+public class Me {
 	public static void main(String[] args) {
 
 		System.out.println("Kris Rahy");
 		System.out.println("Mechatronics Engineering");
+		System.out.println("First Year");
 	}
 }
